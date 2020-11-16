8c9ab1462d708dbb10011c5ab45bebc2acb5f194
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2020-11-16 10:55:19 -0500

    migrating nonce-tracker to typescript (#8)
    * migrating nonce-tracker to typescript
    
    * Changes based on the review suggestion and addition of eslint changes
    
    * deleting .eslintignore
    
    * Fixing the export problem with the NonceTracker class and changes for review suggestion
    
    * changes based on review suggestions

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  extends: [
+    '@metamask/eslint-config',
+    '@metamask/eslint-config/config/nodejs',
+    '@metamask/eslint-config/config/typescript',
+  ],
+  plugins: [
+    'json',
+  ],
+  overrides: [{
+    files: [
+      '*.js',
+      '*.json',
+    ],
+    parserOptions: {
+      sourceType: 'script',
+    },
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+    },
+  }],
+  ignorePatterns: [
+    '!eslintrc.js',
+    'dist/',
+    'node_modules/',
+  ],
+};
